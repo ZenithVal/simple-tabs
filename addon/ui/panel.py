d0d04efd8342fba79e88ed087c90934b54721d75
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2025-01-06 19:54:19 -0500

    Moved into Tool panel

--- addon/ui/panel.py
+++ addon/ui/panel.py
@@ -6,10 +6,9 @@ from .. import icons
 class MainPanel(bpy.types.Panel):
     bl_idname = 'SIMPLETABS_PT_MainPanel'
     bl_label = 'SIMPLE TABS'
-    bl_category = ''
     bl_space_type = 'VIEW_3D'
-    bl_region_type = 'HEADER'
-
+    bl_region_type = 'UI'
+    bl_category = "Tool"
 
     def draw(self, context: bpy.types.Context):
         prefs = utils.addon.prefs()
@@ -17,9 +16,6 @@ class MainPanel(bpy.types.Panel):
 
         layout = self.layout
         column = layout.column()
-
-        if self.is_popover:
-            layout.ui_units_x = prefs.popover_width
 
         row = column.row()
         rows = max(len(prefs.tab_items), 8 if show_move else 6)
@@ -38,11 +34,3 @@ class MainPanel(bpy.types.Panel):
             col.operator('simpletabs.move', text='', icon='TRIA_UP').direction = 'UP'
             col.operator('simpletabs.move', text='', icon='TRIA_DOWN').direction = 'DOWN'
             col.separator()
-        col.operator('wm.url_open', text='', icon_value=icons.id('question')).url = 'http://cw1.me/simpletabd'
-
-
-def popover(self, context: bpy.types.Context):
-    layout = self.layout
-    panel = MainPanel.bl_idname
-    icon = icons.id('simple-tabs')
-    layout.popover(panel, text='', icon_value=icon)
