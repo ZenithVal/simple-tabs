1b223878896feb3e33213e9cc67236206028b32e
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2025-01-06 21:03:32 -0500

    Re-applies sorting in new/opened files

--- addon/timers/__init__.py
+++ addon/timers/__init__.py
@@ -1,7 +1,12 @@
 import bpy
 from . import startup
 from .. import utils
+from bpy.app.handlers import persistent
 
+@persistent
+def load_handler(_):
+    prefs = utils.addon.prefs()
+    bpy.app.timers.register(startup.startup_timer, first_interval=prefs.startup_delay)
 
 def register():
     prefs = utils.addon.prefs()
@@ -9,7 +14,10 @@ def register():
     if not bpy.app.timers.is_registered(startup.startup_timer):
         bpy.app.timers.register(startup.startup_timer, first_interval=prefs.startup_delay)
 
+    bpy.app.handlers.load_post.append(load_handler)
 
 def unregister():
     if bpy.app.timers.is_registered(startup.startup_timer):
         bpy.app.timers.unregister(startup.startup_timer)
+
+    bpy.app.handlers.load_post.remove(load_handler)
